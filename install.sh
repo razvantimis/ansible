404c5a0c5762b174bc3aca7d78fcff147a90d619
Razvan Timis <razvan.timis@8x8.com>
2023-04-11 12:30:56 +0300

    feat: add neovim & zsh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Install homebrew
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+
+# Install ansible
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py --user
 
