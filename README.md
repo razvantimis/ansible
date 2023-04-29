0c5b0263fc487bf84b71ba0d1a2afa1d9779975c
Razvan Timis <r@timis.dev>
2023-04-29 17:58:07 +0300

    fix: add getext for neovim build

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # My personal Ansible setup
 
+
+# Setup
+
+1. Run `chmod +x install.sh`
+2. Run `./install.sh`
+3. Run `ansible-playbook local.yml --ask-become-pass`
+
 ## My frontend setup
 
 - NodeJs : nvm for managing node version 
--- install.sh
+++ install.sh
@@ -3,8 +3,16 @@
 # Install homebrew
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
 
-# Install ansible
+# Install pip
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py --user
+rm get-pip.py
+
+# Install ansible
 
 python3 -m pip install --user ansible
+
+# Create a new zsh terminal
+exec zsh -l
+
+
